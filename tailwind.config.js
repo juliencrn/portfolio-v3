fcc9a3c298c1f77103f3ca8027a1a4e2eaa51c64
Julien <juliencaron@protonmail.com>
2022-01-05 19:40:46 +0100

    chore: Setup tailwindcss

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  content: [
+    './pages/**/*.{js,ts,jsx,tsx}',
+    './components/**/*.{js,ts,jsx,tsx}',
+    './src/styles/**/*.css',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
