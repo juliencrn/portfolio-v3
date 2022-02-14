fb2ba8e52c50cbe2b249fc07e9c468de53156a9f
Julien <juliencaron@protonmail.com>
2022-02-14 05:18:26 +0100

    feat: add tags on project card hover

--- src/components/ProjectCards.tsx
+++ src/components/ProjectCards.tsx
@@ -1,16 +1,36 @@
+import { useRef } from 'react';
+import { useHover } from 'usehooks-ts';
+import cn from 'classnames';
+
 import { Project } from '../types';
 import { ExternalLinkIcon, GithubIcon } from './icons';
 
 export function ProjectSmallCard({
-  title, demoUrl, srcUrl, excerpt,
+  title, demoUrl, srcUrl, excerpt, tags,
 }: Project) {
+  const ref = useRef(null);
+  const isHover = useHover(ref);
   const link = srcUrl || demoUrl;
+
   return (
-    <li className="mb-1">
+    <li ref={ref} className="mb-1 relative">
       <a href={link} className="text-gray-50" target="_blank" rel="noreferrer">
         <h4 className="underline inline">{title}</h4>
       </a>
       <span>{`: ${excerpt}`}</span>
+
+      {tags.length > 0 && (
+        <div
+          className={cn(
+            'absolute -bottom-1 left-0 py-1 px-3',
+            'text-gray-900 left-0 bg-green-400 rounded-2xl shadow-lg',
+            'transform transition-opacity translate-y-full ease-in-out duration-500',
+            isHover ? 'opacity-100 z-10' : 'opacity-0 -z-10',
+          )}
+        >
+          <TagList tags={tags} />
+        </div>
+      )}
     </li>
   );
 }
@@ -37,9 +57,19 @@ export function ProjectCard({
       </div>
 
       <p className="mb-4">{excerpt}</p>
-      <code className="text-sm text-pink-400">
-        {tags.map((tag, i) => `${i > 0 ? ' ' : ''} #${tag}`)}
-      </code>
+      <p className="text-pink-400">
+        <TagList tags={tags} />
+      </p>
     </article>
   );
 }
+
+interface TagProps {
+  tags: string[]
+}
+
+const TagList = ({ tags }: TagProps) => (
+  <code className="text-sm">
+    {tags.map((tag, i) => `${i > 0 ? ' ' : ''} #${tag}`)}
+  </code>
+);
