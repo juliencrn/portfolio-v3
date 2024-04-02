37dcbc513885317442dc7fa5e2c620e611a9fb9e
Julien <juliencaron@protonmail.com>
2024-04-02 14:09:19 +0200

    add umami analytics

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,20 @@
+import Script from 'next/script';
 import '../styles/globals.css';
 import type { AppProps } from 'next/app';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      {process.env.NEXT_PUBLIC_UMAMI_WEBSITE_ID && (
+        <Script
+          src="https://analytics.eu.umami.is/script.js"
+          data-website-id={process.env.NEXT_PUBLIC_UMAMI_WEBSITE_ID}
+          strategy="lazyOnload"
+        />
+      )}
+      <Component {...pageProps} />;
+    </>
+  );
 }
 
 export default MyApp;
